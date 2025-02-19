9a1830891c56b8729ef44b543ad53456a8142823
H3ckboy <boggsc716@gmail.com>
2025-02-18 20:54:48 -0500

    Pnematics

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,14 +5,19 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends TimedRobot { 
   private Command m_autonomousCommand;
 
   private final RobotContainer m_robotContainer;
+ public static Compressor blueyCompressor = new Compressor(PneumaticsModuleType.REVPH);
+public static  PneumaticHub bingoPneumaticHub = new PneumaticHub(35);
 
   public Robot() {
     m_robotContainer = new RobotContainer();
